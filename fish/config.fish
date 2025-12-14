7a297cab103544f99f7b12207610a59857657833
valsucksatcoding <valen_washere@outlook.com>
2025-12-14 19:17:42 +0000

    Delete fish directory

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    set -g fish_greeting ""
-    fastfetch
-    starship init fish | source
-    # command abreviations
-    abbr osu 'Downloads/osu.AppImage & disown'
-    abbr skl 'java -jar Downloads/SKlauncher-3.2.12.jar & disown'
-end
